48196a0855935e20e8834f9670a49a6323caf469
Tyler Bates <Fondlytyler@gmail.com>
2020-05-13 11:25:41 -0700

    pass additional test.

--- Shapes.Tests/ModelTests/Pyramid.Tests.cs
+++ Shapes.Tests/ModelTests/Pyramid.Tests.cs
@@ -28,5 +28,27 @@ namespace Shapes.Tests
       Assert.AreEqual(result, 50);
 
     }
+
+    [TestMethod]
+    public void Pyramid_GetArea2_AreaOfPyramid()
+    {
+      // Arrange
+      Triangle sideOne = new Triangle(7,5);
+     
+      Triangle sideTwo = new Triangle(5,5.2);
+      
+      Triangle sideThree = new Triangle(5.5,5);
+     
+      Triangle sideFour = new Triangle(5.6,5.1);
+      
+      Pyramid testOnePyramid = new Pyramid(sideOne, sideTwo, sideThree, sideFour);
+
+      // Act
+      double result = testOnePyramid.CalculateAreaOfTopPyramid();
+
+      // Assert
+      Assert.AreEqual(result, 58.53);
+
+    }
   }
 }
